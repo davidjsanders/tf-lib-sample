6c901a3fcb50e3afc3c78d83d59e9a69b8eeb138
David Sanders <dsanderscanada@gmail.com>
2020-02-11 21:37:56 -0500

    Update to keep secrets in memory only

--- k8s/templates/bootstrap.sh
+++ k8s/templates/bootstrap.sh
@@ -11,8 +11,36 @@ function checkError() {
     if [ "$1" != "0" ]; then logIt "Error: $1"; fi
 }
 
+logIt "Process arguments"
+ARGUMENTS=`getopt -o a:d:e:u:p: \
+              --long auth_file:,domain_name:,email:,nexus_username:,nexus_password: \
+              -n 'provisioner' -- "$@"`
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$ARGUMENTS"
+
+AUTH_FILE=""
+DOMAIN_NAME=""
+EMAIL=""
+NEXUS_USERNAME=""
+NEXUS_PASSWORD=""
+
+while true; do
+  case "$1" in
+    -a | --auth_file ) AUTH_FILE="$2"; shift 2 ;;
+    -d | --domain_name ) DOMAIN_NAME="$2"; shift 2 ;;
+    -e | --email ) EMAIL="$2"; shift 2 ;;
+    -u | --nexus_username ) NEXUS_USERNAME="$2"; shift 2 ;;
+    -p | --nexus_password ) NEXUS_PASSWORD="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
 logIt "Move inventory to /etc/ansible/hosts"
-cp /home/${admin}/inventory.txt /etc/ansible/hosts
+ cp /home/${admin}/inventory.txt /etc/ansible/hosts
 checkError $?
 
 logIt "Clear existing playbook (if exists)"
@@ -30,6 +58,7 @@ checkError $?
 logIt "Execute playbook"
 sudo -u ${admin} \
     ansible-playbook playbook/k8s-playbook/playbook.yml
+#TODO: ADD EXTRA VARS FOR SECRETS
 checkError $?
 
 logIt "Done (for just now)"
